1058f6e7fbe7dff148b8cd0befb488f7f14f39e7
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-11-28 17:24:06 +0100

    license: 2018-2020
    (2018: includes a lot of
    dcos-dev-prod-analysis)

--- bk-ci-analysis.py
+++ bk-ci-analysis.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2018-2019 Jan-Philip Gehrcke
+# Copyright 2018-2020 Dr. Jan-Philip Gehrcke
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
